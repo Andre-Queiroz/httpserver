17a810ba83f093c10e32ac698541d7c73d778590
André Queiroz <andre.torres@quintoandar.com.br>
2022-11-23 21:47:56 -0300

    removing main class comments

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 import httpserver.web.WebServer;
-import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-import calculatorhttp.CalculatorHttp;
 
 public class Main {
+
     public static void main(String[] args) {
-        /*
-        CalculatorHttp calc = new CalculatorHttp();
-        String input = "1";
-        while (input != "0") {
-            Scanner sc = new Scanner(System.in);
-            input = sc.next();
-            System.out.println(calc.executeOperation(input));
-        }
-        */
         new WebServer(8080).execute();
     }
 }
